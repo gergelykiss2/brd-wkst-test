9ec9772e0ac49d2406bd9ce4cfca9050e7d5d777
Gergely_Kiss2 <gergely_kiss2@epam.com>
2021-10-18 11:01:32 +0200

    Initial commit

---
+++ src/main/java/com/gergelytamas/brdwksttest/BrdWkstTestApplication.java
@@ -0,0 +1,13 @@
+package com.gergelytamas.brdwksttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrdWkstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrdWkstTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gergelytamas/brdwksttest/BrdWkstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gergelytamas.brdwksttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrdWkstTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
